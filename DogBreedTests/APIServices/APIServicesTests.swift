b946fb402e4c3e29aa530399c0481d162f937988
Tiago Pinheiro <tiago.pinheiro@itsector.pt>
2023-07-28 16:12:36 +0100

    +add test file; (finish later)

---
+++ DogBreedTests/APIServices/APIServicesTests.swift
@@ -0,0 +1,23 @@
+//
+//  APIServicesTests.swift
+//  DogBreedTests
+//
+//  Created by Tiago Pinheiro on 28/07/2023.
+//
+
+@testable import DogBreed
+import UIKit
+import XCTest
+
+class APIServicesTests: XCTestCase {
+    
+    private var apiService: APIService
+    
+    override class func setUp() {
+        super.setUp()
+    }
+    
+    override class func tearDown() {
+        super.tearDown()
+    }
+}
